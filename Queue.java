2a166890af01d4ee26713ceb358a9c76f6d8d52a
unknown <rickojn@hotmail.com>
2018-03-10 22:50:18 +0000

    no size limit for queue

--- Queue.java
+++ Queue.java
@@ -1,7 +1,8 @@
 public class Queue {
 
     private int _count;
-    private int[] _members = new int[5];
+    private int _capacity = 5;
+    private int[] _members = new int[_capacity];
     private int _head;
     private int _tail;
 
@@ -14,6 +15,14 @@ public class Queue {
     }
 
     public void enqueue(int item) {
+        if (_tail == _capacity){
+            _capacity = _capacity + 5;
+            int [] largerArray = new int[_capacity];
+            for (int i = 0; i < _tail; i++){
+                largerArray[i] = _members[i];
+            }
+            _members = largerArray;
+        }
         _members[_tail] = item;
         _count++;
         _tail++;
--- QueueTest.java
+++ QueueTest.java
@@ -8,6 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class QueueTest {
     Queue queue;
 
+    private Queue createQueueWith100Members(){
+        for (int i = 0; i < 100; i++){
+            queue.enqueue(i * 10);
+        }
+        return queue;
+    }
+
     @BeforeEach
     void setUp() {
         queue = new Queue();
@@ -49,6 +56,18 @@ class QueueTest {
         assertEquals(queue.dequeue(),300);
         assertEquals(queue.dequeue(),500);
         assertEquals(queue.count(),0);
+    }
+
+    @Test
+    @DisplayName("There should be no limit to the size of the queue")
+    void testQueueNoSizeLimit() {
+        queue =  createQueueWith100Members();
+        assertEquals(queue.dequeue(),0);
+        assertEquals(queue.dequeue(),10);
+        queue.enqueue(500);
+        assertEquals(queue.dequeue(),20);
+        assertEquals(queue.dequeue(),30);
+        assertEquals(queue.count(),97);
     }
 
 }
